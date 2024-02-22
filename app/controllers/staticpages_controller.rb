d43728a4b45c87f0f75b6853b771960a4f885a4b
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-02-22 20:14:29 +0530

    Implemented StaticPagesController to manage the home page. Added a basic form in the home page. Upon form submission, the page reloads to display photos from the specified Flickr user.

---
+++ app/controllers/staticpages_controller.rb
@@ -0,0 +1,24 @@
+class StaticpagesController < ApplicationController
+  require 'flickr'
+  def index
+    begin
+      flickr = Flickr.new 
+    unless params[:user_id].blank?
+      begin
+      @photos = flickr.people.getPhotos :user_id => params[:user_id]
+      rescue Flickr::FlickrError => e
+        handle_standard_error(e)
+      end
+    else
+      @photos = flickr.photos.getRecent
+    end
+    rescue StandardError => e
+      handle_standard_error(e)
+    end
+  end
+
+  def handle_standard_error(error)
+    flash[:alert] = "#{error.class}: #{error.message}"
+    redirect_to root_path
+  end
+end
